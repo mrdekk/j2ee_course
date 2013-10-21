53c77d61c8d57d489b71631bcb6603340861e3e6
mrdekk <mrdekk@yandex.ru>
2013-10-21 12:27:50 +0600

    add: Пример для лекции 3 - JNDI

---
+++ code.samples/lec.3/lec3-war/src/main/java/ru/mrdekk/j2ee/lec3/jndi/ComfortableTemperatureFactory.java
@@ -0,0 +1,37 @@
+package ru.mrdekk.j2ee.lec3.jndi;
+
+import java.util.Hashtable;
+
+import javax.naming.Context;
+import javax.naming.Name;
+import javax.naming.RefAddr;
+import javax.naming.Reference;
+import javax.naming.spi.ObjectFactory;
+
+public class ComfortableTemperatureFactory implements ObjectFactory
+{
+	public Object getObjectInstance( Object obj, Name name, Context nameCtx, Hashtable< ?, ? > environment ) 
+		throws Exception
+	{
+		if ( obj instanceof Reference ) 
+		{
+			Reference ref = ( Reference )obj;
+			
+			if ( ref.getClassName( ).equals( ComfortableTemperature.class.getName( ) ) )  
+			{
+				RefAddr addrRef = ref.get( ComfortableTemperature.REF_ADDR_REF );
+				RefAddr addrTemperature = ref.get( ComfortableTemperature.REF_ADDR_TEMPERATURE );
+				
+				if ( null != addrRef && null != addrTemperature )
+				{
+					return new ComfortableTemperature( 
+						addrRef.getContent( ).toString( ), 
+						Float.parseFloat( addrTemperature.getContent( ).toString( ) ) 
+					);
+				}
+			}
+		}
+		
+		return null;	
+	}
+}
