352b996f380f2a61dc5e6aaa8b1f0be76d2166bd
mrdekk <mrdekk@yandex.ru>
2013-11-18 20:13:46 +0600

    add: Добавлены исходные коды для лекции 7

---
+++ code.samples/lec.7/lec7-war/src/main/java/ru/mrdekk/j2ee/lec7/Obj.java
@@ -0,0 +1,11 @@
+package ru.mrdekk.j2ee.lec7;
+
+public class Obj
+{
+	private String _message;
+	
+	// @formatter:off
+	public void setMessage( String message ) { _message = message; }
+	public String getMessage( ) { return _message; }
+	// @formatter:on
+}
